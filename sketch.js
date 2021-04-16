6480ba42ab2d4e085219af2e2bb3a819d63fc40d
pecheinterdite <63062711+pecheinterdite@users.noreply.github.com>
2021-04-16 16:56:42 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  background(220);
+  textSize(50)
+  fill(255);
+  text('Welcome to Telecation', 30, windowHeight-100)
+}
